ffb78b1786767aa654ef0ad2a9b488ed816e1d3c
annisamaulidazuhriah <annisamaulidazuhriah23@mhs.politala.ac.id>
2024-10-12 23:20:44 +0800

    Menambahkan dan memperbarui file views: app.blade.php

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Java Juice</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}"> <!-- Link ke file CSS Anda -->
+</head>
+<body>
+    @include('partials.navbar') <!-- Navigasi -->
+    @yield('content')
+</body>
+</html>
